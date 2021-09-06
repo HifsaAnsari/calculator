42ba99e54756e9b492d1a2adb7e539fbac5b155e
HifsaAnsari <72120217+HifsaAnsari@users.noreply.github.com>
2021-09-07 01:41:20 +0500

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import React ,{useEffect} from 'react'
+import React ,{useState} from 'react'
 // import React ,{useEffect, useState,useRef} from 'react'
 import './calculator.css'
 function Calculator() {
